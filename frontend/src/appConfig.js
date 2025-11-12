e11d4cb8a88a57fdd17db1a138f2fab6ab1e3745
VISION-3D SENE <ssene07@gmail.com>
2025-11-13 00:57:29 +0100

    deploy: fix dependencies and Vercel config

---
+++ frontend/src/appConfig.js
@@ -0,0 +1,7 @@
+// appConfig.js
+const config = {
+  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  SOCKET_URL: process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000'
+};
+
+export default config;
